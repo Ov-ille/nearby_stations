b9c6b0c95c6d1f1deb66d196bb2f80b6f2dce192
Ov-ille <81677045+Ov-ille@users.noreply.github.com>
2023-11-04 22:30:12 +0100

    Update tests.py

--- departures/tests.py
+++ departures/tests.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import User
 from django.apps import apps
+from departures.models import Stations
 import pytest
 
 # Create your tests here.
@@ -9,3 +9,7 @@ import pytest
 class Test_db:
     def test_models_exist(self):
         assert "Stations" in [x.__name__ for x in apps.get_models()]
+
+    def test_create_station(self):
+        Stations.objects.create(station_name="Test1", station_id=1)
+        assert Stations.objects.filter(station_name="Test1").exists()
