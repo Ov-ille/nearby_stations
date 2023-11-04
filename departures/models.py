5cd606f6f4ebc32cdc730044f0f1ad9a00d666a3
Ov-ille <81677045+Ov-ille@users.noreply.github.com>
2023-11-04 21:56:21 +0100

    create stations model + register + migrate

--- departures/models.py
+++ departures/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Stations(models.Model):
+    station_name = models.CharField(max_length=200)
+    station_id = models.BigIntegerField()
+
+    def __str__(self):
+        return self.station_name
