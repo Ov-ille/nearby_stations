f40f778d07026ccbe6a00cb2fbcb668b302c82b3
Ov-ille <81677045+Ov-ille@users.noreply.github.com>
2023-11-04 21:58:33 +0100

    setup pytest

--- departures/tests.py
+++ departures/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.apps import apps
+import pytest
 
 # Create your tests here.
+
+
+@pytest.mark.django_db
+class Test_db:
+    def test_models_exist(self):
+        assert "Stations" in [x.__name__ for x in apps.get_models()]
