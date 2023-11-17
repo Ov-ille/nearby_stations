da0b21077ac4bab0c13c34c630c8ecc50b69a882
Ov-ille <81677045+Ov-ille@users.noreply.github.com>
2023-11-17 09:12:46 +0100

    extract helper functions

---
+++ departures/helpers.py
@@ -0,0 +1,28 @@
+import requests
+from departures.models import Stations
+
+
+def create_stations(stop):
+    Stations.objects.create(station_name=stop["name"], station_id=stop["id"])
+
+
+def delete_stations_helper():
+    Stations.objects.all().delete()
+
+
+def get_nearby_stations_from_vbb(payload):
+    r = requests.get(
+        "https://v6.vbb.transport.rest/locations/nearby",
+        params=payload,
+        timeout=(0.25, None),
+    )
+    return {"status": r.status_code, "response": r}
+
+
+def get_departures_for_station_from_vbb(station_id):
+    r = requests.get(
+        f"https://v6.vbb.transport.rest/stops/{station_id}/departures",
+        params={"results": 3},
+        timeout=(0.25, None),
+    )
+    return {"status": r.status_code, "response": r}
