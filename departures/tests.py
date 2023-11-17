d18c1d65c0d09a54e41cd403764da9f4b6312021
Ov-ille <81677045+Ov-ille@users.noreply.github.com>
2023-11-17 09:15:20 +0100

    add tests

--- departures/tests.py
+++ departures/tests.py
@@ -1,46 +1,53 @@
 from django.apps import apps
 from departures.models import Stations
 from departures.views import coord
+from departures.helpers import (
+    create_stations,
+    delete_stations_helper,
+    get_nearby_stations_from_vbb,
+    get_departures_for_station_from_vbb,
+)
 import pytest
 import requests
 
+
 # Create your tests here.
-
-
 @pytest.mark.django_db
 class Test_db:
     def test_models_exist(self):
         assert "Stations" in [x.__name__ for x in apps.get_models()]
 
     def test_create_station(self):
-        Stations.objects.create(station_name="Test1", station_id=1)
+        create_stations({"name": "Test1", "id": 1})
         assert Stations.objects.filter(station_name="Test1").exists()
+
+    def test_delete_stations(self):
+        delete_stations_helper()
+        assert len(Stations.objects.all()) == 0
 
 
 @pytest.mark.django_db
-class Test_view_index:
-    def test_station_length(self):
-        stations = Stations.objects
-        assert type(len(stations.all())) is int
+class Test_view_stations:
+    @pytest.fixture
+    def vbb_api_stations(self):
+        return get_nearby_stations_from_vbb({**coord, **{"results": 1}})
 
     @pytest.fixture
-    def vbb_api_response(self):
-        return requests.get(
-            "https://v6.vbb.transport.rest/locations/nearby",
-            params={**coord, **{"results": 1}},
-        )
+    def vbb_api_departures(self, vbb_api_stations):
+        return get_departures_for_station_from_vbb(10)
 
-    def test_vbb_api_status(self, vbb_api_response):
-        assert vbb_api_response.status_code == 200
+    def test_get_nearby_stations_from_vbb_status(self, vbb_api_stations):
+        assert vbb_api_stations["status"] == 200
 
-    def test_add_stations_from_api_response(self, vbb_api_response):
-        list_of_stops = [x["name"] for x in vbb_api_response.json()]
-        for stop in vbb_api_response.json():
-            Stations.objects.create(station_name=stop["name"], station_id=stop["id"])
-        error = False
-        for stop in list_of_stops:
-            if stop not in Stations.objects.all().values_list(
-                "station_name", flat=True
-            ):
-                error = True
-        assert error == False
+    def test_get_nearby_stations_from_vbb_response(self, vbb_api_stations):
+        if vbb_api_stations["status"] == 200:
+            assert all(
+                x in vbb_api_stations["response"].json()[0] for x in ["name", "id"]
+            )
+
+    def test_get_departures_for_station_from_vbb_status(self, vbb_api_departures):
+        assert vbb_api_departures["status"] == 200
+
+    def test_get_departures_for_station_from_vbb_response(self, vbb_api_departures):
+        if vbb_api_departures["status"] == 200:
+            assert "departures" in vbb_api_departures["response"].json()
